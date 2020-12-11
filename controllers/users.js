e83c504ac175b975a354ee6c273a1a7683b19ec3
GneissBytes <59232985+GneissBytes@users.noreply.github.com>
2020-12-11 14:13:19 +0100

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -73,21 +73,17 @@ module.exports.deleteUser = async (req, res, next) => {
 module.exports.changeUser = async (req, res) => {
     const { _id } = req.params;
     const { username, email, new_password, old_password } = req.body;
-
-    try {
-        const user = await User.findByIdAndUpdate(_id, { $set: { username, email } }, { useFindAndModify: false, runValidators: true })
-        if (new_password && old_password) {
-            await user.changePassword(old_password, new_password)
-            if (err) {
-                if (err.name === 'IncorrectPasswordError') {
-                    throw new ExpressError('Incorrect password', 403)
-                } else {
-                    throw new ExpressError('Something went wrong', 403)
-                }
+    
+    const user = await User.findByIdAndUpdate(_id, { $set: { username, email } }, { useFindAndModify: false, runValidators: true })
+    if (new_password && old_password) {
+        await user.changePassword(old_password, new_password)
+        if (err) {
+            if (err.name === 'IncorrectPasswordError') {
+                throw new ExpressError('Incorrect password', 403)
+            } else {
+                throw new ExpressError('Something went wrong', 403)
             }
         }
-    } catch (e) {
-        throw new ExpressError('Username and email must be unique', 403)
     }
 
 
