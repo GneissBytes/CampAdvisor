0c53f6150aa25c345148880e31dd84078e9ad72e
GneissBytes <59232985+GneissBytes@users.noreply.github.com>
2020-12-11 17:32:10 +0100

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -13,7 +13,7 @@ module.exports.submitUser = async (req, res, next) => {
     const registeredUser = await User.register(new User({ email, username }), password)
     req.login(registeredUser, err => {
         if (err) return next(err)
-        req.flash('success', 'Welcome to Yelp Camp')
+        req.flash('success', 'Welcome to CampAdvisor')
         res.redirect('/campgrounds')
     })
 };
